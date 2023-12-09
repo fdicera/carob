6d4b0383900f0a8ec1c769873af8d7916358e321
fdicera <67300276+fdicera@users.noreply.github.com>
2023-12-09 04:59:25 +0000

    Removed uneeded text from challenge run and added home links.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
 
-  def show
-    if current_user.survey_id == nil
-      redirect_to("/surveys")
-    else
-      redirect_to("/challenge_types")
-    end
-  end
+  #def show
+   # if current_user.id == nil
+    #  redirect_to("/surveys")
+    #else
+    #  redirect_to("/challenge_types")
+    #end
+  #end
   
+  render({ :template => "home/show" })
+
 end
