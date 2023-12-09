e3764c7a721215fcbf3de5799e09d1d1a9effd40
fdicera <67300276+fdicera@users.noreply.github.com>
2023-12-09 03:29:12 +0000

    Route to survet if new user, otherwise challenges.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
 
   def show
-
-    if current_user.survey_id.at(0) == nil
+    if current_user.survey_id == nil
       redirect_to("/surveys")
     else
       redirect_to("/challenge_types")
